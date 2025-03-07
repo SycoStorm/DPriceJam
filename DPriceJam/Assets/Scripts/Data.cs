cad48076cc27cd36002345c0f25517fb55cbd60a
Destiney0 <50345556+SycoStorm@users.noreply.github.com>
2025-03-07 11:36:12 -0500

    More script setup and logic

--- DPriceJam/Assets/Scripts/Data.cs
+++ DPriceJam/Assets/Scripts/Data.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData", order = 2)]
 public class Data : ScriptableObject
 {
-    
+    string previousLevel;
+    string currentLevel;
+
+    int turn = 0;
+
+   public int Turn
+    {
+        get
+        {
+            return turn;
+        }
+
+        set  //This keeps keeps the flip turns to only 2 turns no matter what.
+        {
+            if(turn == 0)
+            {
+                turn = 1;
+            }
+            else
+            {
+                turn = 0;
+            }
+
+           
+        }
+    }
 }
