87cb480b34246cc211fb2420dccd7d4e6ff11cb6
Destiney0 <50345556+SycoStorm@users.noreply.github.com>
2025-03-07 11:17:30 -0500

    GameData and Card Data SO

--- DPriceJam/Assets/Scripts/Card.cs
+++ DPriceJam/Assets/Scripts/Card.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[CreateAssetMenu(fileName = "Card", menuName = "ScriptableObjects/NewCard", order = 1)]
 
-public class Card : MonoBehaviour
+public class Card:ScriptableObject  
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
